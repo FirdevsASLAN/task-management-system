1a2ed6920714b8bc898c3760b777c43dc77f9d4a
Firdevs Aslan <firdevsaslan6@gmail.com>
2022-03-02 15:09:10 +0300

    add email input and button on login page

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -4,6 +4,8 @@
       <v-row class="text-center">
         <v-col class="mb-4">
           <h1 class="display-2 font-weight-bold mb-3">Login</h1>
+          <v-text-field v-model="email" label="E-mail"></v-text-field>
+          <v-btn elevation="2" @click="login">Login</v-btn>
         </v-col>
       </v-row>
     </v-container>
@@ -13,5 +15,16 @@
 <script>
 export default {
   name: "LoginView",
+  data() {
+    return {
+      email: "",
+    };
+  },
+  methods: {
+    login() {
+      console.log(this.email);
+      this.email = "";
+    },
+  },
 };
 </script>
